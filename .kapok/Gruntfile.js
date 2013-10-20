ba464f6e8e4873da777a9db5d937ceddda1279cb
TooBug <i@toobug.net>
2013-10-20 21:28:39 +0800

    更新构建过程

--- .kapok/Gruntfile.js
+++ .kapok/Gruntfile.js
@@ -9,26 +9,42 @@ module.exports = function(grunt){
 				},
 				files: [{
 					src: ['**'],
-					cwd: '../src/',
+					cwd: '../tmp/',
 					expand: true,
 					dest: './',
 				}]
 			}
 		},
 		copy:{
+			prepareBuild:{
+				files:[{
+					src:['**'],
+					cwd:'../src',
+					expand:true,
+					dest:'../tmp'
+				}]
+			},
 			nw:{
 				files:[{
 					src:['app.nw'],
 					dest:'../other/kapok-tmpl.app/Contents/Resources/',
 				}]
+			}
+		},
+		clean:{
+			options:{
+				force:true
 			},
+			removeLess:['../tmp/styles/less'],
+			cleanTmp:['../tmp','./app.nw']
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
-	grunt.registerTask('nw', ['compress:nw','copy:nw']);
+	grunt.registerTask('nw', ['copy:prepareBuild','clean:removeLess','compress:nw','copy:nw','clean:cleanTmp']);
 	grunt.registerTask('test', ['compress:nw']);
 
 };
